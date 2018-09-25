ee381f7eb1b9e2a5b5b07313b8797ed1ac7cc7e3
Diogo Eiji Suguimoto <diogo@cobli.co>
2018-09-25 16:20:54 -0300

    Fix svg loader of storybook

--- packages/react-scripts/config/storybook/webpack.config.js
+++ packages/react-scripts/config/storybook/webpack.config.js
@@ -3,13 +3,74 @@
 const webpack = require('webpack');
 
 module.exports = (baseConfig, env, defaultConfig) => {
+  const svgLoader = defaultConfig.module.rules.find(
+    rule => rule.test.source == /\.svg$/.source
+  );
+
+  if (svgLoader) {
+    svgLoader.use = [
+      {
+        loader: svgLoader.loader,
+        options: {
+          noquotes: true,
+        },
+      },
+    ];
+
+    svgLoader.loader = undefined;
+  }
+
   defaultConfig.module.rules.push({
     test: /\.(ts|tsx)$/,
     use: [
+      // This loader parallelizes code compilation, it is optional but
+      // improves compile time on larger projects
       {
-        loader: require.resolve('ts-loader'),
+        loader: require.resolve('thread-loader'),
         options: {
-          transpileOnly: true,
+          poolTimeout: Infinity, // keep workers alive for more effective watch mode
+        },
+      },
+      {
+        loader: require.resolve('babel-loader'),
+        options: {
+          babelrc: false,
+          presets: [
+            [
+              require.resolve('@cobli/babel-preset-react-app'),
+              {
+                flow: false,
+              },
+            ],
+            [require.resolve('@babel/preset-typescript')],
+          ],
+          plugins: [
+            [
+              require.resolve('babel-plugin-emotion'),
+              {
+                autoLabel: true,
+                sourceMap: true,
+                labelFormat: '[filename]--[local]',
+              },
+            ],
+            [
+              require.resolve('@cobli/babel-plugin-named-asset-import'),
+              {
+                loaderMap: {
+                  svg: {
+                    ReactComponent: 'svgr/webpack![path]',
+                  },
+                },
+              },
+            ],
+          ],
+          // This is a feature of `babel-loader` for webpack (not Babel itself).
+          // It enables caching results in ./node_modules/.cache/babel-loader/
+          // directory for faster rebuilds.
+          cacheDirectory: true,
+          // Don't waste time on Gzipping the cache
+          cacheCompression: false,
+          highlightCode: true,
         },
       },
       require.resolve('react-docgen-typescript-loader'),
